bb3294ca582ba1429caafb321668ef52799ab895
vladcheck <valdemarcheck@yandex.ru>
2026-04-21 20:24:00 +0300

    feat: make prisma config

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
